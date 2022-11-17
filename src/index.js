759b4f340d7dfe859ae8aa1f7c6ba7964510b703
francoDavolio <francodavolio12@gmail.com>
2022-11-17 12:01:54 -0300

    hice inicialice los middlewares

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import path from 'path'
 
 const app = express()
 app.set('port', process.env.PORT || 4000)
@@ -7,4 +10,18 @@ app.listen(app.get('port'), () => {
   console.log('Estoy en el puerto' + app.get('port'))
 })
 
-console.log('eoeoeo')
+//middlewares: son funciones que se ejecutan antes de llegar a las rutas
+app.use(cors())
+//permite peticiones remotas
+//permite recibir y usar objetos en formato json
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+//informacion extra
+app.use(morgan('dev'))
+//cargar un archivo estatico
+app.use(express.static(path.join(__dirname, '../public')))
+
+//rutas
+app.get('/', (req, res) => {
+  res.send('hola desde el banckend en la peticion get')
+})
